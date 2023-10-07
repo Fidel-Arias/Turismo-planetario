d57668cd6b67145d9f7646c8608e3db9ad8ef704
Dani <vilcadaniel53@gmail.com>
2023-10-07 13:18:57 -0500

    fix rocket

--- src/components/Game2D.jsx
+++ src/components/Game2D.jsx
@@ -1,11 +1,10 @@
 import { Canvas, useLoader, useThree, useFrame } from "@react-three/fiber";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-import { Box3, DirectionalLight } from "three";
+import { Box3, DirectionalLight, Euler, AnimationMixer, Quaternion, Vector3 } from "three";
 import { OrbitControls, Stars } from "@react-three/drei";
 import { Physics, usePlane, useBox } from "@react-three/cannon";
 import { useRef, useEffect, useState } from "react";
 import "../styles/Game2D.css";
-
 function useKeyboardControls() {
   const [movement, setMovement] = useState({
     forward: false,
@@ -56,33 +55,32 @@ function Model() {
   const box = new Box3().setFromObject(gltf.scene);
   const height = box.max.y - box.min.y;
   gltf.scene.position.y = height / 2;
-  const { forward, backward } = useKeyboardControls();
-  const [ref, api] = useBox(() => ({ mass: 0, position: [0, height / 2, 0] }));
-
+  const keyboardControls = useKeyboardControls();
+  const [ref, api] = useBox(() => ({ mass: 1, position: [0, height / 2, 0] }));
+  const currentDirection = useRef(new Vector3(0, 0, -1));
+  const totalRotation = useRef(0);
   useFrame((state, delta) => {
-    console.log(api);
-    if (forward) {
-      api.velocity.set(0, 0, -1);
-    } else if (backward) {
-      api.velocity.set(0, 0, 1);
-    } else {
-      api.velocity.set(0, 0, 0);
+    let velocity = new Vector3(0, 0, 0);
+    if (keyboardControls.forward) {
+      velocity.add(currentDirection.current.clone().multiplyScalar(100));
+      if (keyboardControls.left) {
+        totalRotation.current += Math.PI / 45;
+        currentDirection.current.applyAxisAngle(new Vector3(0, 1, 0), Math.PI / 45);
+        gltf.scene.rotation.y += Math.PI / 45;
+      } else if (keyboardControls.right) {
+        totalRotation.current -= Math.PI / 45;
+        currentDirection.current.applyAxisAngle(new Vector3(0, 1, 0), -Math.PI / 45);
+        gltf.scene.rotation.y -= Math.PI / 45;
+      }
+    }
+
+    api.velocity.set(velocity.x, velocity.y, velocity.z);
+    if (keyboardControls.forward) {
+      api.rotation.copy(ref.current.rotation);
     }
   });
 
   return <primitive object={gltf.scene} ref={ref} />;
-}
-
-function MyDirectionalLight(props) {
-  const light = useRef();
-  const { scene } = useThree();
-
-  useEffect(() => {
-    scene.add(light.current);
-    return () => scene.remove(light.current);
-  }, [light, scene]);
-
-  return <primitive ref={light} object={new DirectionalLight()} {...props} />;
 }
 
 function InvisibleFloor() {
@@ -103,10 +101,10 @@ export default function Game2D() {
       <Canvas style={{ background: "black" }}>
         <Stars radius={150} depth={50} count={5000} factor={4} saturation={0} fade speed={1} />
         <OrbitControls />
-        <ambientLight intensity={1} />
+        <ambientLight intensity={0.5} />
         <Physics>
           <Model />
-          <InvisibleFloor />
+          {/* <InvisibleFloor /> */}
         </Physics>
       </Canvas>
     </div>
